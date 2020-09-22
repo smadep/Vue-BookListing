d0eb681621335fd87a640d6fd3a7966a3ac531fc
Stefan Rauchegger <s.rauchegger@gmx.at>
2020-09-22 13:04:53 +0200

    step 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>{{title}}</h1>
     <ul>
-        <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+        <book-item v-for="book in books" :book="book"></book-item>
         
     </ul>
   </div>
 </template>
 <script>
+import BookItem from './BookItem';
+
 export default {
   name: "BookList",
   data: function () {
@@ -20,6 +22,9 @@ export default {
         ]
     };
   },
+  components: {
+      BookItem
+  }
 };
 </script>
 <style>
